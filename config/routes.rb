b314bc01f8111c5b9a8c102583c4acd4528f61a7
King Josaphat Chewa <26507383+Kingjosh007@users.noreply.github.com>
2022-02-23 16:57:16 +0100

    Update router file

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   root "users#index"
 
-  get "/users", to: "users#index"
-  get "/users/:id", to: "users#show"
-
-  get "/users/:user_id/posts", to: "user_posts#index"
-  get "/users/:user_id/posts/:id", to: "user_posts#show"
+  resources :users, only: %i[index show] do 
+    resources :posts, only: %i[index show]
+  end
 end
