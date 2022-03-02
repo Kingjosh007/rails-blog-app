022751f810383bc96c6ccdec759b3ada7a66167a
King Josaphat Chewa <26507383+Kingjosh007@users.noreply.github.com>
2022-03-02 10:21:20 +0100

    Create methods update_post_counter and recent_comments in Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,12 @@ class Post < ApplicationRecord
     has_many :likes 
   
     belongs_to :user
+
+    def update_post_counter
+        user.increment!(:posts_counter)
+    end
+    
+    def recent_comments
+        comments.limit(5).order(created_at: :desc)
+    end
 end
