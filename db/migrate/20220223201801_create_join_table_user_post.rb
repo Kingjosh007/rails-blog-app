e71a766b666c0df0d3666d3680f1b815ecc8d7df
King Josaphat Chewa <26507383+Kingjosh007@users.noreply.github.com>
2022-03-09 01:54:33 +0100

    Update migrations order

--- db/migrate/20220223201801_create_join_table_user_post.rb
+++
@@ -1,8 +0,0 @@
-class CreateJoinTableUserPost < ActiveRecord::Migration[7.0]
-  def change
-    create_join_table :users, :posts, column_options: { null: false }, table_name: :likes do |t|
-      t.index :user_id, unique: true
-      t.index :post_id, unique: true
-    end
-  end
-end
