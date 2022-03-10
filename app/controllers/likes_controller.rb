9e3866271587d95fd49a959064af16ba7d2d45a9
King Josaphat Chewa <26507383+Kingjosh007@users.noreply.github.com>
2022-03-11 00:27:06 +0100

    Fix bug in post creation

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,14 +1,14 @@
 class LikesController < ApplicationController
     def create
       @post = Post.find(params[:post_id])
-      @like = @post.likes.create(author_id: current_user.id, post_id: @post.id)
+      @like = @post.likes.create(user_id: current_user.id, post_id: @post.id)
 
 respond_to do |format|
         format.html do
           if @like.save
-            redirect_to user_post_path(@post.author.id, @post.id), notice: 'Liked 👍'
+            redirect_to user_post_path(@post.user.id, @post.id), notice: 'Liked 👍'
           else
-            redirect_to user_post_path(@post.author.id, @post.id), alert: 'Like not added'
+            redirect_to user_post_path(@post.user.id, @post.id), alert: 'An error occured, please try again!'
           end
         end
       end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,7 +25,7 @@ class PostsController < ApplicationController
   respond_to do |format|
       format.html do
         if new_post.save
-          redirect_to user_post_path(new_post.author_id, new_post.id), notice: 'Post created successfully'
+          redirect_to user_post_path(new_post.user_id, new_post.id), notice: 'Post created successfully'
         else
           render :new, alert: 'An error occured. Please try again!'
         end
