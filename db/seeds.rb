814ca1079f3d61475784d644d76747441205ed06
King Josaphat Chewa <26507383+Kingjosh007@users.noreply.github.com>
2022-03-18 00:14:15 +0100

    Comment out the seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,23 +7,23 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 
-# Users
+# # Users
 
-first_user = User.create(name: 'Charlie', photo: '', bio: 'Teacher from Mexico.')
-second_user = User.create(name: 'Lennon', photo: '', bio: 'Teacher from Poland.')
-third_user = User.create(name: 'Angel', photo: '', bio: 'Software Engineer and blogger.')
+# first_user = User.create(name: 'Charlie', photo: '', bio: 'Teacher from Mexico.')
+# second_user = User.create(name: 'Lennon', photo: '', bio: 'Teacher from Poland.')
+# third_user = User.create(name: 'Angel', photo: '', bio: 'Software Engineer and blogger.')
 
-# Posts 
+# # Posts 
 
-first_post = Post.create(user: first_user, title: 'Hello', text: 'This is my first post.')
-second_post = Post.create(user: first_user, title: 'Hello World!', text: 'This is my second post. I plan to write more')
-third_post = Post.create(user: third_user, title: 'Hello Internet', text: 'This is my first post. Is it good?')
-fourth_post = Post.create(user: second_user, title: 'Howdy!', text: 'We love football!')
+# first_post = Post.create(user: first_user, title: 'Hello', text: 'This is my first post.')
+# second_post = Post.create(user: first_user, title: 'Hello World!', text: 'This is my second post. I plan to write more')
+# third_post = Post.create(user: third_user, title: 'Hello Internet', text: 'This is my first post. Is it good?')
+# fourth_post = Post.create(user: second_user, title: 'Howdy!', text: 'We love football!')
 
-# Comments
+# # Comments
 
-Comment.create(post: first_post, author: third_user, text: 'Sure. Great reminder, thanks!')
-Comment.create(post: first_post, user: second_user, text: 'So relevant. Keep it up!')
-Comment.create(post: second_post, user: third_user, text: 'Thank you so much!')
-Comment.create(post: third_post, user: first_user, text: 'So smart... Lorem ipsum sit dolor amit.')
+# Comment.create(post: first_post, user: third_user, text: 'Sure. Great reminder, thanks!')
+# Comment.create(post: first_post, user: second_user, text: 'So relevant. Keep it up!')
+# Comment.create(post: second_post, user: third_user, text: 'Thank you so much!')
+# Comment.create(post: third_post, user: first_user, text: 'So smart... Lorem ipsum sit dolor amit.')
 
